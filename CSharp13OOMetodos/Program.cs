f7c36629e778e949a769ce9f772d48a6011e250c
Jonathan Levy <jonthan.adm2012@gmail.com>
2023-12-03 17:57:14 -0300

    Exemplos de Orientação a Objetos com classe contendo propriedades

--- CSharp13OOMetodos/Program.cs
+++ CSharp13OOMetodos/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharp13OOMetodos;
+using CSharp13OOMetodos.Exemplos;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+/*
+//Intanciado um objeto(exemplo01) da clase Exemplo01 
+exemplo01 exemplo01 = new exemplo01();
+//Chamado o métod Executar do objeto(exemplo01)
+exemplo01.Executar();
+*/
+
+//Instaciar um objeto da clase Exemplo02
+//Exemplo02 exemplo02 = new Exemplo02();
+//exemplo02.Executar();
+
+//Instaciar um objeto da clase Exemplo03
+Exemplo03 exemplo03 = new Exemplo03();
+exemplo03.Executar();

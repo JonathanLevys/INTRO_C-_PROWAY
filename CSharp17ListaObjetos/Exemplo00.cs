1eff001fdc9efb263f2fc452ad3f60fe1052eaee
Jonathan Levy <jonthan.adm2012@gmail.com>
2023-12-17 17:37:54 -0300

    Exemplos de listas de objetos

---
+++ CSharp17ListaObjetos/Exemplo00.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp17ListaObjetos
+{
+    internal class Exemplo00
+    {
+        public void Executar()
+        {
+            List<Aluno> alunos = new List<Aluno>();
+
+            Aluno paulo = new Aluno();
+            paulo.Nome = "Paulo";
+            paulo.Nota1 = 9.4;
+            paulo.Nota2 = 8; 
+            paulo.Nota3 = 7.6;
+
+            alunos.Add(paulo);
+
+            Aluno jose = new Aluno();
+            jose.Nome = "José";
+            jose.Nota1 = 5;
+            jose.Nota2 = 4;
+            jose.Nota3 = 3;
+            alunos.Add(jose);
+
+            //// apresentar o nome do primeiro aluno:
+            //Console.WriteLine("Nome do 1° aluno: " + primeiroAluno.Nome);
+
+            //// Apresentar o nome 
+            //alunos.Remove = jose;
+
+            
+        }
+    }
+}
